cc3076302790e3df8d978a0edfdc7dcc7aad20a5
MikeHalbheer <49029879+MikeHalbheer@users.noreply.github.com>
2022-04-12 14:39:28 +0200

    Documented code

--- main.py
+++ main.py
@@ -25,91 +25,130 @@ import json
 
 def main():
 
+    # Load the config file
     config_file = open('config.json')
     config = json.load(config_file)
     
+    # Load all files that need to be parsed
     parse_files = get_parse_files(config['input_path'])
 
+    # Load all line codes from the Geoterra art code list
     line_codes = get_line_codes(config['line_code_path'])
 
+    # Iterate over all files
     for file in parse_files:
-        sx_job = etree.parse(file) # Reads the XML
-        JOBFile = sx_job.getroot()
-        FieldBook = JOBFile.find('FieldBook')
-        Reductions = JOBFile.find('Reductions')
+        
+        # Load the JXL file as an Element Tree
+        sx_job = etree.parse(file)
+        JOBFile = sx_job.getroot() # Get the file root
+        FieldBook = JOBFile.find('FieldBook') # Get the FieldBook object
+        Reductions = JOBFile.find('Reductions') # Get the Reductions object
 
+        # List of point records
         point_records = []
 
+        # Find all point records in the FieldBook
         for index, record in enumerate(FieldBook):
             if record.tag == 'PointRecord':
                 code_raw = record.find('Code').text
+                # If the point has no code it is ignored
                 if code_raw is None:
                     point_records.append([index, None, None])
                     continue
+                # Split the code into the actual code and the line start indicator
+                # Save the index in the FieldBook element children listfor later access
                 code_split = code_raw.split(' ')
                 point_records.append([index, code_split[0], 1 if len(code_split) > 1 else None])
 
+        # Cast to numpy array for faster operations
         point_records = np.array(point_records)
 
+        # List of reduction records
         reduction_records = []
 
+        # Split all Reduction records in the actual code and the line start indicator
+        # Save the index in the Reductions element children list for later access
         for index, record in enumerate(Reductions):
             code_raw = record.find('Code').text
+            # If a point has no code it is ignored
             if code_raw is None:
                 reduction_records.append([index, None, None])
                 continue
             code_split = code_raw.split(' ')
             reduction_records.append([index, code_split[0], 1 if len(code_split) > 1 else None])
 
+        # Cast to numpy array for faster operations
         reduction_records = np.array(reduction_records)
 
+        # Execute for every line code
         for line_code in line_codes:
+            
+            # Find all point records that belong to the given code
             point_records_code = point_records[point_records[:, 1] == line_code]
+            # Fing all reduction records that belong to the given record
             reduction_records_code = reduction_records[reduction_records[:, 1] == line_code]
+            
+            # If no points for the code were found continue with next code
             if point_records_code.size == 0:
                 continue
+                
+            # Get the indices of the line starts in the FieldBook element
             line_starts_pr = np.array(point_records_code[point_records_code[:, 2] == 1, 0])
             
+            # Get the indices of the line ends in the point record array
             line_end_idx_pr = np.transpose((point_records_code[:, 2] == 1).nonzero()) - 1
             line_end_idx_pr = np.append(line_end_idx_pr, -1)
+            
+            # Get the indices of the line ends in the FieldBook element
             line_ends_pr = point_records_code[line_end_idx_pr[1:], 0]
 
+            # Get the indices of the line start in the Reductions element 
             line_starts_rr = np.array(reduction_records_code[reduction_records_code[:, 2] == 1, 0])
+            
+            # Get the indices of the line ends in the reduction record array
             line_end_idx_rr = np.transpose((reduction_records_code[:, 2] == 1).nonzero()) - 1
             line_end_idx_rr = np.append(line_end_idx_rr, -1)
+            
+            # Get the indices of the line ends in the Reductions element
             line_ends_rr = reduction_records_code[line_end_idx_rr[1:], 0]
 
+            # For all line starts in the point records remove the trailing line start indicator (' S')
             for line_start in line_starts_pr:
                 old_code = FieldBook[line_start].find('Code').text
-                new_code = old_code[:-2]
+                new_code = old_code[:-2] # Remove the indicator
 
                 FieldBook[line_start].find('Code').text = new_code
 
+            # For all line ends in the point records insert the negative indicator
             for line_end in line_ends_pr:
                 old_code = FieldBook[line_end].find('Code').text
                 new_code = '-' + old_code
 
                 FieldBook[line_end].find('Code').text = new_code
 
+            # For all line starts in the reductions records remove the trailing line start indicator (' S')
             for line_start in line_starts_rr:
                 old_code = Reductions[line_start].find('Code').text
                 new_code = old_code[:-2]
 
                 Reductions[line_start].find('Code').text = new_code
-
+            
+            # For all line ends in the reduction records insert the negative indicator
             for line_end in line_ends_rr:
                 old_code = Reductions[line_end].find('Code').text
                 new_code = '-' + old_code
 
                 Reductions[line_end].find('Code').text = new_code
 
+        # Create a new ElementTree object for output
         parsed_jxl = etree.ElementTree(sx_job.getroot())
 
+        # Create the output file name to be identical to the input one
         output_path = os.path.join(config['output_path'], os.path.basename(file))
+        
+        # Write the resulting ElementTree to a new Trimble JobXML file
         parsed_jxl.write(output_path, pretty_print=True)
-
-        print('Finished')
-            
+        
 
 def get_parse_files(dir_input: str) -> list:
     '''
@@ -125,23 +164,28 @@ def get_parse_files(dir_input: str) -> list:
     parse_files : List of strings
         List of files that should be parsed
     '''
-
+    
+    # Catchment list of all files to be parsed
     parse_files = []
-
+    
+    # Find all files in the direcotry
     for file in os.listdir(dir_input):
+        # Ignore any non JobXML file
         if file.endswith('.jxl'):
+            # Get full file path
             file_path = os.path.join(dir_input, file)
             parse_files.append(file_path)
 
     return parse_files
 
-def get_line_codes(dir_line_codes) -> list:
+def get_line_codes(dir_line_codes: str) -> list:
     '''
     Gets all line codes from the current code list
 
     Parameters
     ----------
-    None
+    dir_line_codes : String
+        The directory of the Geoterra code list CSV file
 
     Returns
     -------
@@ -149,8 +193,10 @@ def get_line_codes(dir_line_codes) -> list:
         List containing all line codes in the Geoterra Code list
     '''
 
+    # Read the 'Geoterra_Vorlage.csv'
     code_list = pd.read_csv(dir_line_codes, sep=';', header=0, usecols=['Objektart', 'Messcode'], encoding='unicode_escape')
 
+    # Filter for line elements
     line_codes = code_list.Messcode[code_list.Objektart == 'PL'].to_numpy()
 
     return line_codes
